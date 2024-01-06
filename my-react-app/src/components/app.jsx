ae304bc30e95a1cd22bb3c65735219cf850db1d1
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-06 22:11:28 +0000

    #

--- my-react-app/src/components/app.jsx
+++ my-react-app/src/components/app.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { pyramid } from '../mario'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { pyramid } from '../mario';
+import './App.css';  // Import your CSS file
 
-function app(){
-    <link href="/my-react-app/src/nav.css"></link>
-    <h1>Welcome to my page</h1>
-    <p id='pyra'>{pyramid}</p>
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my page</h1>
+      <div className="container">
+        <ul>
+          <li>
+            <p>First row</p>
+            <p id='pyra'>{pyramid}</p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>Second row</p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>Third row</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
