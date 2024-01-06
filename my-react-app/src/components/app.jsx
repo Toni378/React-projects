532e740f46068a3a80f48691b8804da6692f5cc1
Idriz Atilla <136346643+Idriz4work@users.noreply.github.com>
2024-01-06 22:07:13 +0000

    #

--- my-react-app/src/components/app.jsx
+++ my-react-app/src/components/app.jsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { pyramid } from '../mario'
 
 function app(){
+    <link href="/my-react-app/src/nav.css"></link>
     <h1>Welcome to my page</h1>
+    <p id='pyra'>{pyramid}</p>
 }
